c6ea0e6803c99ce4a5ef0ba8a8bce7395b329a74
Ariff M <munshicorp@gmail.com>
2023-10-28 00:47:20 +0800

    Add router and page components

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Posts from '../pages/Posts';
+import NewPost from '../pages/NewPost';
+import Post from '../pages/Post';
+import EditPost from '../pages/EditPost';
+import PageNotFound from '../pages/PageNotFound';
+
+function AppRoutes() {
+
+    return (
+       <Routes>
+          <Route index element={<Posts />} />
+          <Route path='/posts' element={<Posts />} />
+          <Route path='/posts/new' element={<NewPost />} />
+          <Route path='/posts/:id' element={<Post />} />
+          <Route path='/posts/:id/edit' element={<EditPost />} />
+          <Route path="*" element={<PageNotFound />} />            
+       </Routes>
+    )
+ }
+ 
+ export default AppRoutes;
