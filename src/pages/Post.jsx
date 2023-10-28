392f049cb539f1d9c1cda0fbd76480acbe778499
Ariff M <munshicorp@gmail.com>
2023-10-28 13:30:12 +0800

    Add delete functionality

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 
 function Post() {
     const [post, setPost] = useState({});
     const { id } = useParams();
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch(`http://localhost:8080/api/posts/${id}`)
@@ -11,11 +13,24 @@ function Post() {
             .then(data => setPost(data.data))
     })
 
+    function handleDelete() {
+        fetch(`http://localhost:8080/api/posts/${id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.result === 200) {
+                navigate('/posts')
+            }
+        })
+    }
+
     return (
         <>
             <h1>{post.title}</h1>
             <p>{post.description}</p>
             <img src={post.image} />
+            <button onClick={handleDelete}>Delete</button>
             <div>
                 <Link to={`/posts/${id}/edit`}>Edit Post</Link>
                 <Link to="/posts">Back to Posts</Link>
