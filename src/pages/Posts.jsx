511ae3b9c9183b3f9bf686f66f68999de38721da
Ariff M <munshicorp@gmail.com>
2023-10-28 12:59:37 +0800

    Add posts page functionality

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,6 +1,26 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/posts')
+            .then(res => res.json())
+            .then(data => setPosts(data.data))
+    }, [])
+
     return (
-        <h1>All Posts</h1>
+        <>
+            <h1>All Posts</h1>
+            {posts.length === 0 && <p>Loading...</p>}
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>
+                        <Link to={`/posts/${post.id}`}>{post.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
 
